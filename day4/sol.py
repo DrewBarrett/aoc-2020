fb450573872f64e8d5655d00ccc8dd9579b90cc3
Drew Barrett <dbarret1@umbc.edu>
2020-12-08 23:22:39 -0500

    day4 p2

--- day4/sol.py
+++ day4/sol.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+
+import re
 def p1(arr, optarr):
     valid = 0
     for ids in arr:
@@ -11,23 +13,57 @@ def p1(arr, optarr):
             valid = valid + 1
     return valid
 
-def p1(arr, optarr):
+def p2(arr, optarr):
     valid = 0
+    eyes = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
     for ids in arr:
         isvalid = True
         for req in optarr:
             if ids.get(req) is None:
                 isvalid = False
                 break
-            if int(ids.get(byr)) < 1920 or int(ids.get(byr)) > 2020:
-                isvalid = False
-                break
-            if int(ids.get(iyr)) < 1920 or int(ids.get(iyr)) > 2020:
-                isvalid = False
-                break
-            if int(ids.get(eyr)) < 1920 or int(ids.get(eyr)) > 2020:
-                isvalid = False
-                break
+        if not isvalid:
+            continue
+        if int(ids.get("byr")) < 1920 or int(ids.get("byr")) > 2002:
+            isvalid = False
+            continue
+        if int(ids.get("iyr")) < 2010 or int(ids.get("iyr")) > 2020:
+            isvalid = False
+            continue
+        if int(ids.get("eyr")) < 2020 or int(ids.get("eyr")) > 2030:
+            isvalid = False
+            continue
+        mins = 150
+        most = 193
+        if ids.get("hgt").find("in") != -1:
+            mins = 59
+            most = 76
+        elif ids.get("hgt").find("cm") == -1:
+            isvalid = False
+            continue
+        height = ids.get("hgt")[:-2]
+        height = int(height)
+        if height < mins or height > most:
+            isvalid = False
+            continue
+
+        if ids.get("ecl") not in eyes:
+            isvalid = False
+            continue
+
+        hcl = ids.get("hcl")
+        if re.search('^#[0-9a-f]{6}$', hcl) is None:
+            isvalid = False
+            continue
+
+        pid = ids.get("pid")
+        if re.search('^[0-9]{9}$', pid) is None:
+            isvalid = False
+            continue
+
+        print(ids, isvalid)
+
+
         if isvalid:
             valid = valid + 1
     return valid
@@ -47,6 +83,7 @@ def main():
                 ids[item[0]] = item[1]
 
     print(p1(arr, ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]))
+    print(p2(arr, ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]))
 
 if __name__ == "__main__":
     main()
